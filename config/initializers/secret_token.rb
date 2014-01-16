4d6062fb19c3868b582674533ea626c7cde3cfd3
Ihc <Ihc@gmail.com>
2014-01-16 15:25:32 +0800

    佮rails一開始 丞宏

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Nmtl::Application.config.secret_key_base = 'de4c2820a4d7c14563687340ab313de0fedba0230ae0cd1848d88678aaf89a4836ddd897df2905d0cd92e87f6bd9cb8239417481c228bd0b7ebe8cacb486c1b2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Nmtl::Application.config.session_store :cookie_store, key: '_nmtl_session'
