36d3faedf75874225d2d5598090f19f69f9bec33
Ihc <Ihc@gmail.com>
2014-01-18 15:41:39 +0800

    暫時按呢，量傷濟無法度一條一條改 丞宏

--- app/controllers/kai2_ji7s_controller.rb
+++ app/controllers/kai2_ji7s_controller.rb
@@ -4,7 +4,7 @@ class Kai2Ji7sController < ApplicationController
   # GET /kai2_ji7s
   # GET /kai2_ji7s.json
   def index
-    @kai2_ji7s = Kai2Ji7.all.limit(5)
+    @kai2_ji7s = Kai2Ji7.where('"無齊記號" LIKE ?','%*%*%*%*%*%').order('LENGTH(REPLACE("無齊記號",\'\n\',\'\')) ASC').limit(100)
   end
 
   # GET /kai2_ji7s/1
@@ -41,8 +41,9 @@ class Kai2Ji7sController < ApplicationController
   # PATCH/PUT /kai2_ji7s/1.json
   def update
     respond_to do |format|
+      kai2_ji7_params[:無齊記號].slice! '!'
       if @kai2_ji7.update(kai2_ji7_params)
-        format.html { redirect_to @kai2_ji7, notice: 'Kai2 ji7 was successfully updated.' }
+        format.html { redirect_to edit_kai2_ji7_path(@kai2_ji7), notice: 'Kai2 ji7 was successfully updated.' }
         format.json { head :no_content }
       else
         format.html { render action: 'edit' }
@@ -65,6 +66,7 @@ class Kai2Ji7sController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_kai2_ji7
       @kai2_ji7 = Kai2Ji7.find(params[:id])
+      @kai2_ji7.無齊記號 = '!'+@kai2_ji7.無齊記號
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
